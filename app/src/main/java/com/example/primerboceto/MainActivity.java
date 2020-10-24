0d955bbaea964cbbe67b43a282e8b9ea78ca4051
AlexCampos98 <72565364+AlexCampos98@users.noreply.github.com>
2020-10-24 20:52:42 +0200

    Creacion del layout de la actividad calculadora.

--- app/src/main/java/com/example/primerboceto/MainActivity.java
+++ app/src/main/java/com/example/primerboceto/MainActivity.java
@@ -19,45 +19,41 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        Intent i = new Intent();
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.ofCalculadora) {
-            i = new Intent(this, CalendarActivity.class);
-        } else if (id == R.id.ofPiePapTij) {
-            i = new Intent(this, CalendarActivity.class);
-        } else if (id == R.id.ofCerrarSesion) {
-            i = new Intent(this, CalendarActivity.class);
-        }
-        startActivity(i);
-
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//
+//        //Obtencion del identificador de la opcion escogida en el actionBar.
+//        int id = item.getItemId();
+//
+//        //Creacion de la variable de cambio de actividad.
+//        Intent i = new Intent();
+//
+//        //Filtro de inicio de una de las actividades seleccionadas.
+//        if (id == R.id.ofCalculadora) {
+//            i = new Intent(this, CalendarActivity.class);
+//        } else if (id == R.id.ofPiePapTij) {
+//            i = new Intent(this, CalendarActivity.class);
+//        } else if (id == R.id.ofCerrarSesion) {
+//            i = new Intent(this, CalendarActivity.class);
+//        }
+//        startActivity(i);
+//
+//        return true;
+//    }
 
 }
